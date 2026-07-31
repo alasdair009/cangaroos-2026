ffbc641918fd6a859bd0bf2123ffddbf52f63f16
Ali Macrae <alasdair.macrae@jagex.com>
2026-07-31 11:55:34 +0100

    chore: remove next font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
